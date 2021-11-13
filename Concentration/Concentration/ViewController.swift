37c960da6a687bef900bdd6dc0cac5032e0c0365
Chereshnev <chereshnev.n.s@edu.mirea.ru>
2021-11-13 14:32:10 +0300

    Лекция 1
    Сделал 4 карточки с животными, также реализовал логику переворачивания карточки

--- Concentration/Concentration/ViewController.swift
+++ Concentration/Concentration/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var touches: Int = 0 {
+        didSet {
+            touchLabel.text = "Touches: \(touches)"
+        }
     }
-
-
+    
+    func flipButton(emoji: String, button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = UIColor.systemBlue
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.titleLabel!.font = UIFont(name: "Times New Roman", size: 50)
+            button.backgroundColor = UIColor.white
+        }
+    }
+    
+    let emojiCollectiion = ["🦔", "🐓", "🦔", "🐓"]
+    @IBOutlet var buttonCollection: [UIButton]!
+    @IBOutlet weak var touchLabel: UILabel!
+    @IBAction func buttonAction(_ sender: UIButton) {
+        touches += 1
+        if let buttonIndex = buttonCollection.firstIndex(of: sender) {
+            flipButton(emoji: emojiCollectiion[buttonIndex], button: sender)
+        }
+    }
 }
 
